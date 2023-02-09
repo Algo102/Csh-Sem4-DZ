15e14b850b306cc2501b3e330edb341e1dc848e7
Algo <algor102@gmail.comm>
2023-02-09 23:02:39 +0500

    Упростил, добавив жестко элементы от 100 до 1000

---
+++ se4.2-arr100-999/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами
+int ReadInt (string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i]+" ");
+   Console.WriteLine();
+}
+
+
+int lengt = ReadInt("Enter the number of elements in the array");
+
+int[] array = new int[lengt];
+  for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(100, 1000);
+
+PrintArray(array);
