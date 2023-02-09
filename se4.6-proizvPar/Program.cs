07e0fc39962c2285262d044e75eae4b82b07ecfd
Algo <algor102@gmail.comm>
2023-02-10 01:15:01 +0500

    Произведение пар в массиве

---
+++ se4.6-proizvPar/Program.cs
@@ -0,0 +1,38 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+int ReadInt (string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int[] GreatArrayRand(int lengt, int left, int right)
+{
+  int[] array = new int[lengt];
+  for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(left, right+1);
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i]+" ");
+   Console.WriteLine();
+}
+
+
+int length = ReadInt("Enter the number of elements in the array");
+int left = ReadInt("Enter the lower value of the element");
+int right = ReadInt("Enter the higher value of the element");
+
+int[]array = GreatArrayRand(length, left, right);
+
+PrintArray(array);
+
+int a = 0;
+for (int i = 0; i < length/2; i++)
+{
+  Console.WriteLine($"{i}) {array[i]} * {array[length-1-a]} = {array[i]*(array[length-1-a])}");
+  a++;
+}
+
