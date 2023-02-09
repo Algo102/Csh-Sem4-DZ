d295c9a424aed314051190c2142ee0571acf3f8a
Algo <algor102@gmail.comm>
2023-02-09 22:44:17 +0500

    Задат массив с 0 и 1 и вывести

---
+++ se4.1-array8/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int ReadInt (string message)
+{
+  Console.WriteLine(message);
+  return int.Parse(Console.ReadLine()!);
+}
+
+int[] GreatArrayRand(int lengt, int left, int right)
+{
+  int[] array = new int[lengt];
+  for(int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(left, right+1);
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i]+" ");
+   Console.WriteLine();
+}
+
+
+int lengt = ReadInt("Enter the number of elements in the array");
+int left = ReadInt("Enter the lower value of the element");
+int right = ReadInt("Enter the higher value of the element");
+
+int[]array = GreatArrayRand(lengt, left, right);
+
+PrintArray(array);
